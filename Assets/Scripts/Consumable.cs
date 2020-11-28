75925b09f44b3616ca104d4936c591d164a63170
JimTheGhost <63484552+JimTheGhost@users.noreply.github.com>
2020-11-28 10:01:24 -0500

    Consumables
    you cna heal now

---
+++ Assets/Scripts/Consumable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Consumable : MonoBehaviour
+{
+    [SerializeField] private int healValue = 5;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var target = other.GetComponent<PlayerHealth>();
+        if (target != null)
+        {
+            target.ApplyHealing(healValue); 
+        }
+}
+}
